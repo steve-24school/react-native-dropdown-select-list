91e34bc1f52082d65a8777bfacfeccea58a286a3
steve-24school <cherian@blitzcodesys.com>
2025-03-25 04:30:48 +1100

    Update SelectList.tsx

--- components/SelectList.tsx
+++ components/SelectList.tsx
@@ -172,7 +172,7 @@ const SelectList: React.FC<SelectListProps> =  ({
                     </View>
                 :
                     <TouchableOpacity style={[styles.wrapper,boxStyles]} onPress={() => { if(!dropdown){ Keyboard.dismiss(); slidedown() }else{ slideup() } }}>
-                        <Text  placeholderTextColor={Platform.OS === 'ios' ? '#C7C7CD' : '#808080'} style={[{fontFamily},inputStyles]}>{ (selectedval == "") ? (placeholder) ? placeholder : 'Select option' : selectedval  }</Text>
+                        <Text style={[{ fontFamily },inputStyles,selectedval === "" && { color: Platform.OS === "ios" ? "#C7C7CD" : "#808080" }]}>{ (selectedval == "") ? (placeholder) ? placeholder : 'Select option' : selectedval  }</Text>
                         {
                             (!arrowicon)
                             ?
